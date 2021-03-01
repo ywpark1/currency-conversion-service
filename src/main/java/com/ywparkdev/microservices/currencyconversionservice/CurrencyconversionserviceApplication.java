35d304ddbd07944360ea98f102ea9c0e29fb7f60
Yeon Woo Park <ywoo1234@gmail.com>
2021-02-28 23:17:30 -0500

    Initial commit

---
+++ src/main/java/com/ywparkdev/microservices/currencyconversionservice/CurrencyconversionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ywparkdev.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyconversionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyconversionserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ywparkdev/microservices/currencyconversionservice/CurrencyconversionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywparkdev.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyconversionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
